21f99fd6fc8bf7c196a3b0c6d1a7ef5426599ebc
Ricardo Bautista <rickyhaswifi@gmail.com>
2019-09-03 20:52:25 -0600

    finished app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-    has_many :enrollments
+    has_many :enrollments, dependent: :destroy
     has_many :courses, through: :enrollments
   
     validates :first_name, :last_name, presence: true
-
+  
     def full_name
-        "#{self.first_name} #{self.last_name}"
-      end
-      
+      "#{self.first_name} #{self.last_name}"
+    end
   end
